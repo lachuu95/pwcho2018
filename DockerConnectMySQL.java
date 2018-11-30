61be697947824444b1b965cd560ace5f8215f230
lachuu95 <45293402+lachuu95@users.noreply.github.com>
2018-11-30 18:51:09 +0100

    Update DockerConnectMySQL.java

--- DockerConnectMySQL.java
+++ DockerConnectMySQL.java
@@ -10,15 +10,10 @@ public class DockerConnectMySQL {
    public static void main(String[] args) {
    Connection conn = null;
    Statement stmt = null;
-	   do
-	   try{
-		   Class.forName("com.mysql.jdbc.Driver");
+   try{
+	   Class.forName("com.mysql.jdbc.Driver");
 		   System.out.println("Connecting to database...");
       conn = DriverManager.getConnection(DB_URL,USER,PASS);
-	   }catch(SQLException se){
-      se.printStackTrace();}
-	   while(se==0)
-   try{
       stmt = conn.createStatement();
       String sql;
       sql = "CREATE TABLE Persons (PersonID int, LastName varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255) );";
